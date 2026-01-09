eb4c0a7271f384a8c6c2df49ef5435c270cd901e
solomon <solohlemons75@gmail.com>
2026-01-09 20:22:04 +0300

    Changed Common-dtos

--- src/main/java/com/shared/dtos/CatSubmissionDto.java
+++ src/main/java/com/shared/dtos/CatSubmissionDto.java
@@ -1,4 +1,24 @@
 package com.shared.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CatSubmissionDto {
+
+    private String studentAdmissionId;
+    private SubmissionType submissionType;
+    private int courseId;
+    private int catId;
+    private String className;
+    private List<AnswerDto> answerText;
+    private LocalDateTime submissionDate;
+
+
 }
--- src/main/java/com/shared/dtos/QuizSubmissionDto.java
+++ src/main/java/com/shared/dtos/QuizSubmissionDto.java
@@ -1,4 +1,22 @@
 package com.shared.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class QuizSubmissionDto {
+    private String studentAdmissionId;
+    private SubmissionType submissionType;
+    private int courseId;
+    private int quizId;
+    private int moduleId;
+    private String className;
+    private List<AnswerDto> answerText;
+    private LocalDateTime submissionDate;
+
 }
